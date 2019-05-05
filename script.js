e97f1aaa95e872e45f84fbe6ae68bab370630d43
Rafał Nowakowski <gee@vp.pl>
2019-05-05 17:46:33 +0200

    Add counters

--- script.js
+++ script.js
@@ -1,102 +1,104 @@
-var app = React.createElement(App);
-ReactDOM.render(app, document.getElementById('app'));
-
-/*
-//dane filmow
-var movies = [
-  {
-    id: 1,
-    title: 'Harry Potter',
-    desc: 'film o czarodzieju',
-	img: 'https://ssl-gfx.filmweb.pl/po/05/71/30571/7529392.6.jpg'
-  },
-  {
-    id: 2,
-    title: 'Król Lew',
-    desc: 'Film o królu sawanny',
-	img: 'https://ssl-gfx.filmweb.pl/po/68/78/6878/7389475.6.jpg'
-  },
-  {
-    id: 3,
-    title: 'Breaking Bad',
-    desc: 'Gdy nauczyciel chemii dowiaduje się, że ma raka, postanawia rozpocząć produkcję metamfetaminy, by finansowo zabezpieczyć swoją rodzinę.',
-	img: 'https://ssl-gfx.filmweb.pl/po/06/68/430668/7730445.6.jpg'
-  },
-  {
-    id: 4,
-    title: 'Suits',
-    desc: 'Dzięki swojej fotograficznej pamięci i ponadprzeciętnej inteligencji Mike Ross zdobywa pracę u Harveya Spectera. Jednak firma, w której pracuje, zatrudnia jedynie prawników z Uniwersytetu Harvarda.',
-	img: 'https://ssl-gfx.filmweb.pl/po/57/26/585726/7383459.6.jpg'
-  }
-];
-
-//klasa MovieTitle
-var MovieTitle = React.createClass({
+//klasa Counter
+var Counter = React.createClass({
 	propTypes: {
-		title: React.PropTypes.string.isRequired,
+		start: React.PropTypes.number.isRequired,
+		id: React.PropTypes.number.isRequired,
 	},
+
+	getInitialState: function(){
+		console.log('getInitialState');
+		return { counter: this.props.start };
+	},
+
+	increment: function() {
+        this.setState({
+            counter: this.state.counter + 1
+        });
+    },
+
+	decrement: function() {
+        this.setState({
+            counter: this.state.counter - 1
+        });
+    },
 
 	render: function() {
+		console.log('render');
 		return (
-			React.createElement('h2', {}, this.props.title)
-		);
-	},
-});
-
-//klasa MovieDescription
-var MovieDescription = React.createClass({
-	propTypes: {
-		desc: React.PropTypes.string.isRequired,
-	},
-
-	render: function() {
-		return (
-			React.createElement('p', {}, this.props.desc)
-		);
-	},
-});
-
-//klasa Movie
-var Movie = React.createClass({
-	propTypes: {
-		movieData: React.PropTypes.object.isRequired,
-	},
-
-	render: function() {
-		return (
-			React.createElement('li', {},
-				React.createElement(MovieTitle, {title: this.props.movieData.title}),
-				React.createElement(MovieDescription, {desc: this.props.movieData.desc}),
-				React.createElement('img', {src: this.props.movieData.img})
+			React.createElement('div', {className: 'counter'},
+				React.createElement('span', {}, 'Licznik ' + this.props.id +': ' + this.state.counter),
+				React.createElement('button', {onClick: this.increment}, '+'),
+				React.createElement('button', {onClick: this.decrement}, '-'),
 			)
-		)
+		);
+	},
+
+	//metody lifecycle
+	getDefaultProps: function() {
+		console.log('getDefaultProps');
+	},
+	componentWillMount: function() {
+		console.log('componentWillMount');
+		console.log('-->Wczytywanie danych z serwera');
+	},
+	componentDidMount: function() {
+		console.log('componentDidMount');
+	},
+	componentWillReceiveProps: function(nextProps) {
+		console.log('componentWillReceiveProps');
+		console.log(nextProps);
+	},
+	shouldComponentUpdate: function(nextProps, nextState) {
+		console.log('shouldComponentUpdate');
+		console.log(nextProps);
+		console.log(nextState);
+		return true;
+	},
+	componentWillUpdate: function(nextProps, nextState) {
+		console.log('componentWillUpdate');
+		console.log(nextProps);
+		console.log(nextState);
+	},
+	componentDidUpdate: function(prevProps, prevState) {
+		console.log('componentDidUpdate');
+		console.log(prevProps);
+		console.log(prevState);
+	},
+	componentWillUnmount: function() {
+		console.log('componentWillUnmount');
 	},
 });
 
-//klasa MovieList
-var MovieList = React.createClass({
+//klasa CounterList
+var CounterList = React.createClass({
 	propTypes: {
 		items: React.PropTypes.array.isRequired,
 	},
 
 	render: function() {
+		var elements = this.props.items.map(function(item){
+			return React.createElement(Counter, {key: item.id, id: item.id, start: item.start});
+		});
+
 		return (
-			React.createElement('div', {},
-				React.createElement('h1', {}, 'Lista filmów'),
-			    React.createElement('ul', {}, this.props.items)
-			)
+			React.createElement('div', {}, elements)
 		);
 	},
 });
 
-var element = React.createElement(
-	MovieList,
+var counters = [
 	{
-		items: movies.map(function(movie) {
-			return React.createElement(Movie, {key: movie.id, movieData: movie});
-		})
-	}
-);
-
+		id: 1,
+		start: 0
+	},
+	{
+		id: 2,
+		start: 5
+	},
+	{
+		id: 3,
+		start: -8
+	},
+];
+var element = React.createElement(CounterList, {items: counters});
 ReactDOM.render(element, document.getElementById('app'));
-*/
