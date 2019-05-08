fd1dbd2b7d0b596b1d1fdcad04f3b244fc2c4fe0
Rafał Nowakowski <gee@vp.pl>
2019-05-08 20:51:36 +0200

    Add gif search

--- components/App.js
+++ components/App.js
@@ -1,40 +1,61 @@
-//dane kontaktow
-var contacts = [
-  {
-    id: 1,
-    firstName: 'Jan',
-    lastName: 'Nowak',
-    email: 'jan.nowak@example.com',
-  },
-  {
-    id: 2,
-    firstName: 'Adam',
-    lastName: 'Kowalski',
-    email: 'adam.kowalski@example.com',
-  },
-  {
-    id: 3,
-    firstName: 'Zbigniew',
-    lastName: 'Koziol',
-    email: 'zbigniew.koziol@example.com',
-  }
-];
+var GIPHY_API_URL = 'https://api.giphy.com';
+var GIPHY_PUB_KEY = 'iC4wuxobjYWhw8650X4UkBIW5stFpOZb';
 
-//dane formularza
-var contactForm = {
-  firstName: '',
-  lastName: '',
-  email: ''
-};
-
-//klasa App
 App = React.createClass({
-	render: function() {
-		return (
-			<div className={'app'}>
-				<ContactForm contact={contactForm}></ContactForm>
-				<Contacts items={contacts}></Contacts>
+    render: function() {
+
+        var styles = {
+            margin: '0 auto',
+            textAlign: 'center',
+            width: '90%'
+        };
+
+        return (
+			<div style={styles}>
+			    <h1>Wyszukiwarka GIFow!</h1>
+			    <p>Znajdź gifa na <a href='http://giphy.com'>giphy</a>. Naciskaj enter, aby pobrać kolejne gify.</p>
+			    <Search onSearch={this.handleSearch}/>
+				<Gif
+					loading={this.state.loading}
+				    url={this.state.gif.url}
+				    sourceUrl={this.state.gif.sourceUrl}
+				/>
 			</div>
-		)
+        );
+    },
+	getInitialState: function() {
+		return {
+	        loading: false,
+	        searchingText: '',
+	        gif: {}
+	    };
+	},
+	handleSearch: function(searchingText) { 
+		this.setState({
+			loading: true
+		});
+		this.getGif(searchingText, function(gif) {
+			this.setState({
+				loading: false,
+				gif: gif,
+				searchingText: searchingText
+			});
+		}.bind(this));
+	},
+	getGif: function(searchingText, callback) {
+	    var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
+	    var xhr = new XMLHttpRequest();
+	    xhr.open('GET', url);
+	    xhr.onload = function() {
+	        if (xhr.status === 200) {
+	           	var data = JSON.parse(xhr.responseText).data;
+				var gif = {
+				    url: data.fixed_width_downsampled_url,
+				    sourceUrl: data.url
+				};
+				callback(gif);
+	        }
+	    };
+	    xhr.send();
 	},
 });
